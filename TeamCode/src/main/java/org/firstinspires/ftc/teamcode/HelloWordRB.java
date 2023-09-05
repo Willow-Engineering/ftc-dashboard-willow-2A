c7a331772eca5ca201696e8dc77b98a3f2f58df2
JKBain <joseph_bain.stu@lusherschool.org>
2023-09-05 10:07:39 -0500

    Hello world RB

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWordRB.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class HelloWordRB extends OpMode {
+    @Override
+    public void init() {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        telemetry.addData("Hello", "WorldRB");
+    }@Override
+    public void loop(){
+
+    }
+}
