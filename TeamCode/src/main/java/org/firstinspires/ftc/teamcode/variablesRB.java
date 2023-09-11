53b448a308d78faeb7cc7af09defdca121a5b695
JKBain <joseph_bain.stu@lusherschool.org>
2023-09-11 10:14:23 -0500

    variables rb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/variablesRB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/variablesRB.java
@@ -25,6 +25,12 @@ public class variablesRB extends OpMode {
 
 
     public void loop(){
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("A button", gamepad1.a);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right stick y", gamepad1.right_stick_y);
+        telemetry.addData("B button", gamepad1.b);
 
     }
 }
